aba9baaa788f98058b4cb9b7f78a2a767be87fce
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2020-04-03 14:43:30 +0700

    Add Soal 2

---
+++ soal2/tapclient.c
@@ -0,0 +1,367 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <termios.h>
+
+#define PORT 8080
+
+pthread_t tid[1];
+pthread_t yid[3];
+
+// Getch Template
+
+static struct termios old, new;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); //grab old terminal i/o settings
+  new = old; //make new settings same as old settings
+  new.c_lflag &= ~ICANON; //disable buffered i/o
+  new.c_lflag &= echo ? ECHO : ~ECHO; //set echo mode
+  tcsetattr(0, TCSANOW, &new); //apply terminal io settings
+}
+ 
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+ 
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+ 
+/* 
+Read 1 character without echo 
+getch() function definition.
+*/
+char getch(void) 
+{
+  return getch_(0);
+}
+
+// Getch Template
+
+
+struct sockaddr_in address;
+int sock = 0, valread;
+struct sockaddr_in serv_addr;
+char username[55], password[55], command[55];
+char buffer[1024] = {0};
+int isFound = 0, firsttry = 1, health = 100;
+
+void* theTapTap(void *arg)
+{
+    pthread_t id = pthread_self();
+
+    if(pthread_equal(id, yid[0])) // The tappers
+    {
+        printf("\e[1;1H\e[2J");
+        printf("Game starting in 3 seconds ...");
+
+        int j;
+
+        for(j = 3; j > 0; j--)
+        {
+            printf("%d\n", j);
+            sleep(1);
+        }
+
+        printf("Game starts now! Tap tap as fast as you can!\n");
+        printf("Press [SPACE] to tap.\n");
+
+        char space;
+
+        while(1) // loop to input the space.
+        {
+            if(health == 0)
+            {
+                break;
+            }
+
+            space = getch();
+
+            if(space == 32)
+            {
+                printf("Hit!\n");
+                send(sock, "hit", strlen("hit"), 0);
+            }
+        }
+
+        return 0;
+    }
+    else if(pthread_equal(id, yid[1])) // Health Counter
+    {
+        while(1)
+        {   
+            if(firsttry != 1)
+            {
+                valread = read(sock, buffer, 0);
+
+                if(strcmp(buffer, "hit") == 0)
+                {
+                    health = health - 10;
+                    printf("You've been hit! Your current health : %d\n", health);
+                    memset(buffer, 0, sizeof(buffer));
+                }
+                else if(strcmp(buffer, "win") == 0)
+                {
+                    printf("You win!\n");
+                    memset(buffer, 0, sizeof(buffer));
+                }
+
+                if(health == 0)
+                {
+                    send(sock, "win", strlen("win"), 0);
+                    printf("You lose!\n");
+                    break;
+                }
+            }
+            
+        }
+
+        return 0;
+    }
+}
+
+void* theGame(void *arg)
+{   
+    pthread_t id = pthread_self();
+
+    if(pthread_equal(id, tid[0])) // the game
+    {
+        char cmd[7];
+
+        while(1)
+        {
+            printf("\e[1;1H\e[2J"); // clear screen
+            printf("Welcome to TapTap!\nHere's a list of command:\n1. find -> to find match.\n2. logout -> to logout and exit the game.\n\n");
+            printf("Please enter a command: ");
+            scanf("%s", cmd);
+
+            if(strcmp(cmd, "logout") == 0)
+            {
+                printf("Thank you for playing!\n");
+                sleep(2);
+                 
+                return 0;
+            }
+            else if(strcmp(cmd, "find") == 0)
+            {
+                while(1)
+                {
+                    printf("Finding match for you ...\n");
+                    send(sock, "find", strlen("find"), 0); // sending find message for server
+
+                    if(isFound == 1)
+                    {
+                        break;
+                    }
+
+                    memset(buffer, 0, sizeof(buffer));
+                    sleep(10); 
+                }
+                
+
+                if(isFound == 1)
+                {
+                    isFound = 0;
+                    printf("Match found! Entering the game ...\n");
+                    sleep(2);
+
+                    int error, a = 0;
+
+                    while(a < 2)
+                    {
+                        error = pthread_create(&(yid[a]), NULL, &theTapTap, NULL); // Creating Threads
+
+                        if(error != 0)
+                        {
+                            printf("Can't create threads : [%s]", strerror(error));
+                        }
+
+                        a++;
+                    }
+                    
+                    pthread_join(yid[0], NULL);
+                    pthread_join(yid[1], NULL);
+                    memset(buffer, 0, sizeof(buffer));
+                }
+            }
+        }
+    }
+    else if(pthread_equal(id, tid[1]))
+    {
+        while(1)
+        {   
+            valread = read(sock, buffer, 0);
+            if(strcmp(buffer, "matchfound") == 0)
+            {
+                printf("Match Found.\n");
+                isFound = 1;
+                break;
+            }
+        }
+
+        return 0;
+    }   
+}
+
+int main(int argc, char const *argv[])
+{
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+      
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    int err;
+
+    int loginattempt = 0;
+    int option = 1;
+    char optionstr[5];
+
+    printf("\e[1;1H\e[2J");
+    printf("Welcome to the TapTap game.\n");
+    printf("This is a very simple TapTap game.\n");
+
+    while(1) // login & register loop
+    {   
+        if(option == 1) // after login
+        {
+            if(strcmp(buffer, "true") == 0 && loginattempt > 0) // IF CREDENTIALS ARE TRUE
+            {
+                printf("Login successful!\n\n\n");
+                memset(buffer, 0, sizeof(buffer));
+                
+                int i = 0;
+
+                while(i < 2)
+                {
+                    err = pthread_create(&(tid[i]), NULL, &theGame, NULL); // Creating Threads
+
+                    if(err != 0)
+                    {
+                        printf("Can't create threads : [%s]", strerror(err));
+                    }
+
+                    i++;
+                }
+                
+                pthread_join(tid[0], NULL);
+                pthread_join(tid[1], NULL);
+
+                printf("\e[1;1H\e[2J");
+                printf("Welcome to the TapTap game.\n");
+                printf("This is a very simple TapTap game.\n");
+            }
+            else if(loginattempt > 0) // IF CREDENTIALS ARE FALSE
+            {
+                printf("Login failed. Please try again.\n\n");
+                memset(buffer, 0, sizeof(buffer));
+            }
+        }
+        else if(option == 2)
+        {
+            memset(buffer, 0, sizeof(buffer));
+            option = 1;
+        }
+
+        printf("Select option:\n");
+        printf("1. Login\n");
+        printf("2. Register\n");
+        printf("3. Exit\n\n");
+        printf("Input your selection (1,2,3): ");
+        scanf("%d", &option);
+
+        memset(optionstr, 0, sizeof(optionstr)); // resetting option
+        sprintf(optionstr, "%d", option);
+        send(sock, optionstr, strlen(optionstr), 0);
+
+        if(option == 1) // login option
+        {
+            printf("\n----------------L O G I N----------------\n");
+            printf("Enter your username: ");
+            scanf("%s", username);
+            send(sock, username, strlen(username), 0);
+
+            printf("Enter your password: ");
+            scanf("%s", password);
+            send(sock, password, strlen(password), 0);
+            printf("---------------------------------------\n");
+            printf("Your credentials are now sent to the server.\n");
+            printf(".\n");
+            printf(".\n\n");
+
+            sleep(1);
+            send(sock, "next", strlen("next"), 0);
+            valread = read( sock , buffer, 1024);
+            loginattempt++;
+        }
+        else if(option == 2) // REGISTER
+        {
+            if(strcmp(buffer, "true") == 0)
+            {
+                printf("Register successful! Please login now.\n\n\n");
+            }
+
+            printf("\n----------------R E G I S T E R----------------\n");
+            printf("Enter your username: ");
+            scanf("%s", username);
+            //printf("%s\n", username);
+            send(sock, username, strlen(username), 0);
+
+            printf("Enter your password: ");
+            scanf("%s", password);
+            send(sock, password, strlen(password), 0);
+            printf("---------------------------------------\n");
+            printf("Your credentials are now sent to the server.\n");
+            printf(".\n");
+            printf(".\n\n");
+
+            sleep(1);
+            send(sock, "next", strlen("next"), 0);
+            valread = read( sock , buffer, 1024);
+        }
+        else if(option == 3)
+        {
+            printf("Exiting the program...\n");
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            printf("Wrong input. Please try again.\n\n");
+        }
+    }
+
+    return 0;
+}
---
+++ soal2/tapserver.c
@@ -0,0 +1,367 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <semaphore.h>
+
+#define PORT 8080
+
+pthread_t tid[2];
+
+int server_fd, new_socket, valread, x, activity, max_clients = 2, client_socket[2], max_sd, sd;
+struct sockaddr_in address;
+int opt = 1, option;
+int addrlen = sizeof(address);
+char buffer[1024] = {0};
+char username[55], password[55], integer[1000], optionstr[3];
+char *truecon = "true", *falsecon = "false", *okay = "Command processed.";
+int isHit = 0, isFinding = 0;
+sem_t mutex;
+
+void *theGame(void *arg)
+{
+    sem_wait(&mutex);
+
+    pthread_t id = pthread_self();
+
+    memset(buffer, 0, sizeof(buffer));
+
+    if(pthread_equal(id, tid[0])) // From client
+    {
+        while(1) //SERVER PROGRAM -> MULTI-THREADED
+        {
+            valread = read( new_socket , buffer, 1024);
+            if(strcmp(buffer, "logout") == 0) // logout
+            {
+                printf("User logged out.\n");
+                memset(buffer, 0, sizeof(buffer));
+                return 0;
+            }
+            else if(strcmp(buffer, "find") == 0) // found match
+            {
+                printf("User is trying to find a match.\n");
+                isFinding = 1;
+                sem_post(&mutex);
+                memset(buffer, 0, sizeof(buffer));
+            }
+
+            if(strcmp(buffer, "hit") == 0)
+            {
+                isHit = 1;
+                memset(buffer, 0, sizeof(buffer));
+            }
+
+            if(strcmp(buffer, "win") == 0)
+            {
+
+            }
+
+            memset(buffer, 0, sizeof(buffer));
+        }
+
+        return 0;
+    }
+    else if(pthread_equal(id, tid[1])) // To Client
+    {
+        printf("Mutex\n");
+        while(1)
+        {
+            if(isHit == 1)
+            {
+                send(new_socket, "hit", strlen("hit"), 0);
+                isHit = 0;
+            }
+
+            if(isFinding == 1)
+            {
+                //sleep(5);
+                sem_wait(&mutex);
+                send(new_socket, "matchfound", strlen("matchfound"), 0);
+                isFinding = 0;
+                sem_post(&mutex);
+            }
+        }
+
+        return 0;
+    }
+
+
+}
+
+int makeFile(char username[55], char password[55], int option) // Function to make a file. Option = Login/Register (1/2). Return 1 if true, 0 if false.
+{
+    int i;
+    FILE *fp; // File Pointer
+    char temp[512];
+    char str[200];
+
+    if(option == 1) // Login -> Read from file
+    {
+        fp = fopen("akun.txt", "r"); //r -> read
+
+        strcpy(str, "username=");
+        strcat(str, username);
+        strcat(str, ";password=");
+        strcat(str, password);
+        strcat(str, ";\n");
+
+        while(fgets(temp, 512, fp) != NULL) // Comparing the data with one in the textfile
+        {
+            if((strcmp(temp, str)) == 0) // If match found.
+            {
+                fclose(fp);
+                return 1; // True. Match found.
+            }
+        }
+        fclose(fp);
+
+        return 0; // If no match found.
+    }
+    else if(option == 2) // Register -> Write to file
+    {
+        fp = fopen("akun.txt", "a"); //w -> write
+
+        strcpy(str, "username=");
+        strcat(str, username);
+        strcat(str, ";password=");
+        strcat(str, password);
+        strcat(str, ";\n");
+
+        printf("%s\n", str);
+
+        for (i = 0; str[i] != '\n'; i++)
+        {
+          /* write to file using fputc() function */
+          fputc(str[i], fp);
+        }
+        fputs("\n",fp);
+        fclose(fp);
+
+        return 1;
+    }
+    
+    return 0; // ERROR
+}
+
+int main(int argc, char const *argv[])
+{   
+    sem_init(&mutex, 0, 1);  
+    fd_set readfds;
+
+    for (x = 0; x < max_clients; x++)   
+    {   
+        client_socket[x] = 0;   
+    }  
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+      
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    
+
+    while(1) // WAITING FOR MULTIPLE CONNECTION
+    {
+        FD_ZERO(&readfds);
+
+        FD_SET(server_fd, &readfds);
+        max_sd = server_fd;
+
+        for ( x = 0 ; x < max_clients ; x++)   
+        {   
+            //socket descriptor  
+            sd = client_socket[x];   
+                 
+            //if valid socket descriptor then add to read list  
+            if(sd > 0)  
+                FD_SET(sd , &readfds);   
+                 
+            //highest file descriptor number, need it for the select function  
+            if(sd > max_sd)   
+                max_sd = sd;   
+        }
+
+        //wait for an activity on one of the sockets , timeout is NULL ,  
+        //so wait indefinitely  
+        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);   
+       
+        if ((activity < 0) && (errno!=EINTR))   
+        {   
+            printf("select error");   
+        }
+
+        if(FD_ISSET(server_fd, &readfds)) // Incoming Connection
+        {
+            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+
+            //inform user of socket number - used in send and receive commands
+            printf("New connection , socket fd is %d , ip is : %s , port : %d  \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+
+            //add new socket to array of sockets
+            for (x = 0; x < max_clients; x++)   
+            {   
+                //if position is empty  
+                if( client_socket[x] == 0 )   
+                {   
+                    client_socket[x] = new_socket;   
+                    printf("Adding to list of sockets as %d\n" , x);   
+                         
+                    break;   
+                }   
+            }
+
+            while(1) // waiting for credentials
+            {
+                valread = read( new_socket , buffer, 1024);
+                strcpy(optionstr, buffer);
+                memset(buffer, 0, sizeof(buffer));
+                if(strcmp(optionstr, "1") == 0)
+                {
+                    printf("User %d selected login option.\n", new_socket);
+                    option = 1;
+                }
+                else if(strcmp(optionstr, "2") == 0)
+                {
+                    printf("User %d selected register option.\n", new_socket);
+                    option = 2;
+                }
+                else if(strcmp(optionstr, "3") == 0)
+                {
+                    printf("User %d exitted.\n", new_socket);
+                    memset(buffer, 0, sizeof(buffer));
+                    exit(EXIT_SUCCESS);
+                }
+
+                valread = read( new_socket , buffer, 1024);
+                strcpy(username, buffer);
+                printf("User %d inputted username.\n", new_socket);
+                memset(buffer, 0, sizeof(buffer));
+
+                valread = read( new_socket , buffer, 1024);
+                strcpy(password, buffer);
+                printf("User %d inputted password.\n", new_socket);
+                memset(buffer, 0, sizeof(buffer));
+
+                valread = read(new_socket, buffer, 1024);
+                if(strcmp(buffer, "next") == 0) // next step
+                {
+                    if(option == 1) // login
+                    {
+                        //printf("Username: %s Password: %s\n", username, password);
+                
+                        if(makeFile(username, password, 1) == 1) // if true
+                        {
+                            send(new_socket , truecon , strlen(truecon) , 0 );
+                            printf("User %d: Authentication successful.\n\n\n", new_socket);
+                            
+                            //Creating thread for the game.
+                            int i = 0, err;
+                            
+                            while(i < 2)
+                            {
+                                err = pthread_create(&(tid[i]), NULL, &theGame, NULL); // Creating Threads
+
+                                if(err != 0)
+                                {
+                                    printf("Can't create threads : [%s]", strerror(err));
+                                }
+
+                                i++;
+                            }
+
+                            pthread_join(tid[0], NULL);
+                            pthread_join(tid[1], NULL);
+                        }
+                        else
+                        {
+                            send(new_socket, falsecon, strlen(falsecon), 0);
+                            printf("User %d: Authentication failed. Trying again...\n", new_socket);
+                        }
+                    }
+                    else if(option == 2) // register
+                    {
+                        //printf("Username: %s Password: %s\n", username, password);
+
+                        if(makeFile(username, password, 2) == 1) // if true
+                        {
+                            send(new_socket , truecon , strlen(truecon) , 0 );
+                            printf("User %d: Registration successful.\n\n\n", new_socket);
+                        }
+                        else
+                        {
+                            send(new_socket, falsecon, strlen(falsecon), 0);
+                            printf("User %d: Registration failed. Trying again...\n",new_socket);
+                        }
+                    }
+                    else // error
+                    {
+                        exit(EXIT_FAILURE);
+                    }            
+                }
+                memset(buffer, 0, sizeof(buffer));
+            }
+        }
+        
+        for (x = 0; x < max_clients; x++)   
+        {   
+            sd = client_socket[x];   
+                 
+            if (FD_ISSET( sd , &readfds))   
+            {   
+                //Check if it was for closing , and also read the  
+                //incoming message  
+                if ((valread = read( sd , buffer, 1024)) == 0)   
+                {   
+                    //Somebody disconnected , get his details and print  
+                    getpeername(sd , (struct sockaddr*)&address ,(socklen_t*)&addrlen);   
+                    printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));   
+                         
+                    //Close the socket and mark as 0 in list for reuse  
+                    close( sd );   
+                    client_socket[x] = 0;   
+                }   
+                     
+                //Echo back the message that came in  
+                else 
+                {   
+                    //set the string terminating NULL byte on the end  
+                    //of the data read  
+                    buffer[valread] = '\0';   
+                    send(sd , buffer , strlen(buffer) , 0 );   
+                }   
+            }   
+        }   
+    }
+
+    sem_destroy(&mutex);
+    return 0;
+}
